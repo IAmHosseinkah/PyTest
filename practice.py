390715fe9322650df3fa918f66a9905879a5b6e1
Nouri <noureddinhosseinkhah@gmail.com>
2024-12-19 19:51:20 +0330

    Initial commit

---
+++ practice.py
@@ -0,0 +1,12 @@
+# Noureddin Hosseinkhah 403442096
+
+print('All the Even numbers: ')
+for i in range(1, 101):
+    if i % 2 == 0:
+        print(i, end=' ')
+
+print('\n')
+print('All the Odd numbers: ')
+for i in range(1, 101):
+    if i % 2 != 0:
+        print(i, end=' ')
